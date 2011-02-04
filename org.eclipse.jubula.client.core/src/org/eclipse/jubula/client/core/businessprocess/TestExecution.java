6fe5c5d392a19f566a3b2fcc0df85fede25fcb5b
Markus Tiede <Markus.Tiede@bredex.de>
2011-02-04 10:11:55 +0100

    Refacotred nextCap retrieval - centralized traverser access.

--- org.eclipse.jubula.client.core/src/org/eclipse/jubula/client/core/businessprocess/TestExecution.java
+++ org.eclipse.jubula.client.core/src/org/eclipse/jubula/client/core/businessprocess/TestExecution.java
@@ -44,7 +44,6 @@ import org.eclipse.jubula.client.core.communication.BaseConnection.NotConnectedE
 import org.eclipse.jubula.client.core.communication.ConnectionException;
 import org.eclipse.jubula.client.core.communication.ServerConnection;
 import org.eclipse.jubula.client.core.i18n.Messages;
-import org.eclipse.jubula.client.core.model.LogicComponentNotManagedException;
 import org.eclipse.jubula.client.core.model.IAUTConfigPO;
 import org.eclipse.jubula.client.core.model.IAUTConfigPO.ActivationMethod;
 import org.eclipse.jubula.client.core.model.IAUTMainPO;
@@ -58,6 +57,7 @@ import org.eclipse.jubula.client.core.model.ITDManagerPO;
 import org.eclipse.jubula.client.core.model.ITestDataPO;
 import org.eclipse.jubula.client.core.model.ITestJobPO;
 import org.eclipse.jubula.client.core.model.ITestSuitePO;
+import org.eclipse.jubula.client.core.model.LogicComponentNotManagedException;
 import org.eclipse.jubula.client.core.model.ReentryProperty;
 import org.eclipse.jubula.client.core.model.ResultTreeBuilder;
 import org.eclipse.jubula.client.core.model.ResultTreeTracker;
@@ -88,8 +88,8 @@ import org.eclipse.jubula.tools.constants.StringConstants;
 import org.eclipse.jubula.tools.constants.TimeoutConstants;
 import org.eclipse.jubula.tools.constants.TimingConstantsClient;
 import org.eclipse.jubula.tools.exception.CommunicationException;
-import org.eclipse.jubula.tools.exception.JBException;
 import org.eclipse.jubula.tools.exception.InvalidDataException;
+import org.eclipse.jubula.tools.exception.JBException;
 import org.eclipse.jubula.tools.messagehandling.MessageIDs;
 import org.eclipse.jubula.tools.objects.IComponentIdentifier;
 import org.eclipse.jubula.tools.objects.event.EventFactory;
@@ -936,20 +936,10 @@ public class TestExecution {
                 }
                 if (testOk) {
                     resultNode.setResult(m_trav.getSuccessResult(), null);
-                    try {
-                        if (!m_stopped) {
-                            nextCap = m_trav.next();
-                        }
-                    } catch (JBException e) {
-                        LOG.error(Messages.IncompleteTestdata, e);
-                        fireError(e);
-                    }
                 } else {
                     // ErrorEvent has occured
                     TestErrorEvent event = msg.getTestErrorEvent();
-                    String eventType = event.getId();
-
-                    if (m_trav.getEventHandlerReentry(eventType).equals(
+                    if (m_trav.getEventHandlerReentry(event.getId()).equals(
                             ReentryProperty.RETRY)) {
                         resultNode.setResult(TestResultNode.RETRYING, event);
                     } else {
@@ -963,15 +953,19 @@ public class TestExecution {
                             pauseExecution(PauseMode.PAUSE);
                         }
                     }
+                }
+
+                if (!m_stopped) {
                     try {
-                        if (!m_stopped) {
-                            nextCap = m_trav.next(eventType);
-                        }
+                        nextCap = testOk ? m_trav.next() : m_trav.next(msg
+                                .getTestErrorEvent().getId());
                     } catch (JBException e) {
                         LOG.error(Messages.IncompleteTestdata, e);
                         fireError(e);
                     }
                 }
+                
+                
                 if (nextCap != null) {
                     while (isPaused()) {
                         try {
