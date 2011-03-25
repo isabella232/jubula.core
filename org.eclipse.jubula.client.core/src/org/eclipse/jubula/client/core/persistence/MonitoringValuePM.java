141d8b1ea800ee725448f93e83cbd7230950f87c
Markus Tiede <Markus.Tiede@bredex.de>
2011-03-25 10:50:35 +0100

    Fix for potential NPE when switching between multiple database instances.

--- org.eclipse.jubula.client.core/src/org/eclipse/jubula/client/core/persistence/MonitoringValuePM.java
+++ org.eclipse.jubula.client.core/src/org/eclipse/jubula/client/core/persistence/MonitoringValuePM.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2010 BREDEX GmbH.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BREDEX GmbH - initial API and implementation and/or initial documentation
+ *******************************************************************************/
 package org.eclipse.jubula.client.core.persistence;
 
 import java.util.Map;
@@ -5,41 +15,46 @@ import java.util.Map;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 
+import org.apache.commons.collections.MapUtils;
 import org.eclipse.jubula.client.core.model.ITestResultSummaryPO;
 import org.eclipse.jubula.client.core.model.PoMaker;
 import org.eclipse.jubula.tools.objects.IMonitoringValue;
 
 /**
  * Loads monitoring values from the database
- * @author marc
- *
+ * 
+ * @author BREDEX GmbH
+ * @created 10.10.2010
  */
 public class MonitoringValuePM {
-    
+
     /** to prevent instantiation */
     private MonitoringValuePM() {
-        //DO NOTHING
-    }    
+        // DO NOTHING
+    }
+
     /**
      * 
-     * @param summaryID The current selected summaryID
-     * @return The monitored values 
-     */    
-    public static final Map<String, IMonitoringValue> 
-    loadMonitoringValues(Object summaryID) {
-        
+     * @param summaryID
+     *            The current selected summaryID
+     * @return The monitored values
+     */
+    public static final Map<String, IMonitoringValue> loadMonitoringValues(
+            Object summaryID) {
+        if (Hibernator.instance() == null) {
+            return MapUtils.EMPTY_MAP;
+        }
         EntityManager session = Hibernator.instance().openSession();
-        ITestResultSummaryPO summary;        
+        ITestResultSummaryPO summary;
         try {
-            EntityTransaction tx = 
-                Hibernator.instance().getTransaction(session);
+            EntityTransaction tx = Hibernator.instance()
+                    .getTransaction(session);
             summary = (ITestResultSummaryPO)session.find(
-                        PoMaker.getTestResultSummaryClass(), summaryID);  
+                    PoMaker.getTestResultSummaryClass(), summaryID);
         } finally {
             Hibernator.instance().dropSession(session);
-        }                
+        }
         return summary.getMonitoringValues();
-    }    
-    
-    
+    }
+
 }
