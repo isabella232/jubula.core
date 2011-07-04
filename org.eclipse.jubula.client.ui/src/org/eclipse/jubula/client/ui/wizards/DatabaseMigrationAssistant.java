9897dfa577fdae2ed56739ce51e66f2b53ed0ca5
Markus Tiede <Markus.Tiede@bredex.de>
2011-07-04 13:47:57 +0200

    Timebox task - re-enable the database migration assistant UI.

--- org.eclipse.jubula.client.ui/src/org/eclipse/jubula/client/ui/wizards/DatabaseMigrationAssistant.java
+++ org.eclipse.jubula.client.ui/src/org/eclipse/jubula/client/ui/wizards/DatabaseMigrationAssistant.java
@@ -13,9 +13,9 @@ package org.eclipse.jubula.client.ui.wizards;
 import java.lang.reflect.InvocationTargetException;
 
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.jubula.client.core.errorhandling.IDatabaseVersionErrorHandler;
 import org.eclipse.jubula.client.core.persistence.Hibernator;
 import org.eclipse.jubula.client.ui.Plugin;
@@ -76,9 +76,10 @@ public class DatabaseMigrationAssistant extends Wizard
         Plugin.getDisplay().syncExec(new Runnable() {
             
             public void run() {
-                MessageDialog.openInformation(Plugin.getShell(), 
-                        Messages.DatabaseMigrationAssistantIntroPageTitle, 
-                        Messages.DatabaseMigrationAssistantIntroPageText);
+                WizardDialog dialog = new WizardDialog(Plugin.getShell(),
+                        DatabaseMigrationAssistant.this);
+                dialog.setHelpAvailable(true);
+                dialog.open();
             }
         });
         
@@ -91,6 +92,9 @@ public class DatabaseMigrationAssistant extends Wizard
     public void addPages() {
         super.addPages();
         addPage(new DatabaseMigrationAssistantIntroPage(INTRO_PAGE_ID));
+        m_importProjectsPage = 
+            new ImportProjectsWizardPage(IMPORT_PROJECTS_PAGE_ID);
+        addPage(m_importProjectsPage);
     }
 
     /**
