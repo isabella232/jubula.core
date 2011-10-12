b264c4234119834d9cb464e6f7ab37551e5f2f06
Zeb Ford-Reitz <Zeb.Ford-Reitz@bredex.de>
2011-10-12 11:29:14 +0200

    Adds o.e.j.c.ui.rap: RAP-specific implementations for Jubula UI.

---
+++ org.eclipse.jubula.client.ui.rap/src/org/eclipse/jubula/client/ui/rap/handlers/project/RapSelectDatabaseHandler.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2011 BREDEX GmbH.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BREDEX GmbH - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+package org.eclipse.jubula.client.ui.rap.handlers.project;
+
+import org.eclipse.jubula.client.ui.handlers.project.AbstractSelectDatabaseHandler;
+
+/**
+ *
+ * @author BREDEX GmbH
+ * @created Oct 05, 2011
+ */
+public class RapSelectDatabaseHandler extends AbstractSelectDatabaseHandler {
+
+    @Override
+    protected void clearClient() {
+        // no-op
+    }
+
+    @Override
+    protected void writeLineToConsole(String line) {
+        // no-op
+    }
+
+}
