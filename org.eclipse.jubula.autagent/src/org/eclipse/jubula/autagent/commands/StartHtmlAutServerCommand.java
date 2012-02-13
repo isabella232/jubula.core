4b4f1c26def24e23dc9ea8afd848ea1672d0c567
Zeb Ford-Reitz <Zeb.Ford-Reitz@bredex.de>
2012-02-13 11:18:45 +0100

    Additional work for https://bugs.eclipse.org/bugs/show_bug.cgi?id=370807.
    Simplifies the logging library situation by removing all references to
    Commons Logging and Log4J.

--- org.eclipse.jubula.autagent/src/org/eclipse/jubula/autagent/commands/StartHtmlAutServerCommand.java
+++ org.eclipse.jubula.autagent/src/org/eclipse/jubula/autagent/commands/StartHtmlAutServerCommand.java
@@ -141,7 +141,6 @@ public class StartHtmlAutServerCommand extends AbstractStartToolkitAut {
                               CommandConstants.TOOLS_BUNDLE_ID,
                               CommandConstants.COMMUNICATION_BUNDLE_ID,
                               CommandConstants.RC_COMMON_BUNDLE_ID,
-                              CommandConstants.SLF4J_JCL_BUNDLE_ID,
                               CommandConstants.SLF4J_API_BUNDLE_ID,
                               CommandConstants.LOGBACK_CLASSIC_BUNDLE_ID,
                               CommandConstants.LOGBACK_CORE_BUNDLE_ID,
