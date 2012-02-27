dd0de52857ddf0f004d9a0ac8eabfdf9e7148ed6
Markus Tiede <Markus.Tiede@bredex.de>
2012-02-27 11:21:11 +0100

    Minor fix - avoid usage of non-accessible API.

--- org.eclipse.jubula.client.ui/src/org/eclipse/jubula/client/ui/preferences/PreferenceInitializer.java
+++ org.eclipse.jubula.client.ui/src/org/eclipse/jubula/client/ui/preferences/PreferenceInitializer.java
@@ -13,8 +13,8 @@ package org.eclipse.jubula.client.ui.preferences;
 import org.apache.commons.codec.binary.Base64;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
-import org.eclipse.core.runtime.preferences.DefaultScope;
-import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jubula.client.ui.Plugin;
 import org.eclipse.jubula.client.ui.constants.Constants;
 import org.eclipse.jubula.tools.constants.ConfigurationConstants;
 import org.eclipse.jubula.tools.constants.StringConstants;
@@ -33,24 +33,23 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer {
 
     @Override
     public void initializeDefaultPreferences() {
-        IEclipsePreferences prefNode = 
-                DefaultScope.INSTANCE.getNode(Constants.PLUGIN_ID);
+        IPreferenceStore prefStore = Plugin.getDefault().getPreferenceStore();
 
-        initializeDefaultPreferencesJubulaBasic(prefNode);
-        initializeDefaultPreferencesObjectMapping(prefNode);
-        initializeDefaultPreferencesKeyBoardShortCuts(prefNode);
-        initializeDefaultPreferencesObservation(prefNode);
-        initializeDefaultPreferencesTestResults(prefNode);
+        initializeDefaultPreferencesJubulaBasic(prefStore);
+        initializeDefaultPreferencesObjectMapping(prefStore);
+        initializeDefaultPreferencesKeyBoardShortCuts(prefStore);
+        initializeDefaultPreferencesObservation(prefStore);
+        initializeDefaultPreferencesTestResults(prefStore);
 
-        prefNode.putBoolean(Constants.ASKSTOPAUT_KEY,
+        prefStore.setDefault(Constants.ASKSTOPAUT_KEY,
                 Constants.ASKSTOPAUT_KEY_DEFAULT);
-        prefNode.put(Constants.USER_KEY, Constants.USER_DEFAULT);
-        prefNode.put(Constants.SCHEMA_KEY, Constants.SCHEMA_DEFAULT);
-        prefNode.putBoolean(Constants.LINK_WITH_EDITOR_TCVIEW_KEY, 
+        prefStore.setDefault(Constants.USER_KEY, Constants.USER_DEFAULT);
+        prefStore.setDefault(Constants.SCHEMA_KEY, Constants.SCHEMA_DEFAULT);
+        prefStore.setDefault(Constants.LINK_WITH_EDITOR_TCVIEW_KEY, 
                 Constants.LINK_WITH_EDITOR_TCVIEW_KEY_DEFAULT);
-        prefNode.putBoolean(Constants.DATADIR_WS_KEY, 
+        prefStore.setDefault(Constants.DATADIR_WS_KEY, 
                 Constants.DATADIR_WS_KEY_DEFAULT);
-        prefNode.put(Constants.DATADIR_PATH_KEY, 
+        prefStore.setDefault(Constants.DATADIR_PATH_KEY, 
                 Platform.getLocation().toOSString());
         
         StringBuilder serverValuesBuilder = new StringBuilder();
@@ -60,117 +59,115 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer {
         serverValuesBuilder.append(new String(
             Base64.encodeBase64(String.valueOf(
                 ConfigurationConstants.AUT_AGENT_DEFAULT_PORT).getBytes())));
-        prefNode.put(
+        prefStore.setDefault(
                 Constants.SERVER_SETTINGS_KEY, 
                 serverValuesBuilder.toString());
     }
 
     /**
      * initialize the default preferences for a preference page 
-     * @param prefNode Preference node.
+     * @param prefStore Preference store
      */
     private static void initializeDefaultPreferencesJubulaBasic(
-            IEclipsePreferences prefNode) {
-        prefNode.putBoolean(Constants.TREEAUTOSCROLL_KEY,
+            IPreferenceStore prefStore) {
+        prefStore.setDefault(Constants.TREEAUTOSCROLL_KEY,
                 Constants.TREEAUTOSCROLL_KEY_DEFAULT);
-        prefNode.putBoolean(Constants.MINIMIZEONSUITESTART_KEY,
+        prefStore.setDefault(Constants.MINIMIZEONSUITESTART_KEY,
                 Constants.MINIMIZEONSUITESTART_KEY_DEFAULT);
-        prefNode.putBoolean(Constants.SHOWORIGINALNAME_KEY, 
+        prefStore.setDefault(Constants.SHOWORIGINALNAME_KEY, 
                 Constants.SHOWORIGINALNAME_KEY_DEFAULT);
-        prefNode.putInt(Constants.PERSP_CHANGE_KEY,
+        prefStore.setDefault(Constants.PERSP_CHANGE_KEY,
                 Constants.PERSP_CHANGE_KEY_DEFAULT);
-        prefNode.putBoolean(Constants.NODE_INSERT_KEY,
+        prefStore.setDefault(Constants.NODE_INSERT_KEY,
                 Constants.NODE_INSERT_KEY_DEFAULT);
-        prefNode.putBoolean(Constants.SHOWCAPINFO_KEY,
+        prefStore.setDefault(Constants.SHOWCAPINFO_KEY,
                 Constants.SHOWCAPINFO_KEY_DEFAULT);
-        prefNode.putBoolean(Constants.SHOW_TRANSIENT_CHILDREN_KEY,
+        prefStore.setDefault(Constants.SHOW_TRANSIENT_CHILDREN_KEY,
                 Constants.SHOW_TRANSIENT_CHILDREN_KEY_DEFAULT);
-        prefNode.putBoolean(Constants.REMEMBER_KEY,
+        prefStore.setDefault(Constants.REMEMBER_KEY,
                 Constants.REMEMBER_KEY_DEFAULT);
     }
     
     /**
      * initialize the default preferences for a preference page 
-     * @param prefNode Preference node.
+     * @param prefStore Preference store
      */
     private static void initializeDefaultPreferencesTestResults(
-            IEclipsePreferences prefNode) {
-        prefNode.putBoolean(Constants.GENERATEREPORT_KEY,
+            IPreferenceStore prefStore) {
+        prefStore.setDefault(Constants.GENERATEREPORT_KEY,
                 Constants.GENERATEREPORT_KEY_DEFAULT);
-        prefNode.put(Constants.REPORTGENERATORSTYLE_KEY,
+        prefStore.setDefault(Constants.REPORTGENERATORSTYLE_KEY,
             Constants.REPORTGENERATORSTYLE_KEY_DEFAULT);
-        prefNode.putBoolean(Constants.OPENRESULTVIEW_KEY,
+        prefStore.setDefault(Constants.OPENRESULTVIEW_KEY,
                 Constants.OPENRESULTVIEW_KEY_DEFAULT);
-        prefNode.putBoolean(Constants.TRACKRESULTS_KEY,
+        prefStore.setDefault(Constants.TRACKRESULTS_KEY,
                 Constants.TRACKRESULTS_KEY_DEFAULT);
-        prefNode.put(Constants.RESULTPATH_KEY,
+        prefStore.setDefault(Constants.RESULTPATH_KEY,
                 Constants.RESULTPATH_KEY_DEFAULT);
-        prefNode.putInt(Constants.MAX_NUMBER_OF_DAYS_KEY,
+        prefStore.setDefault(Constants.MAX_NUMBER_OF_DAYS_KEY,
                 Constants.MAX_NUMBER_OF_DAYS_KEY_DEFAULT);
-        prefNode.putBoolean(Constants.AUTO_SCREENSHOT_KEY, 
+        prefStore.setDefault(Constants.AUTO_SCREENSHOT_KEY, 
                 Constants.AUTO_SCREENSHOT_KEY_DEFAULT);
-        prefNode.putInt(Constants.TEST_EXEC_RELEVANT,
+        prefStore.setDefault(Constants.TEST_EXEC_RELEVANT,
                 Constants.TEST_EXECUTION_RELEVANT_DEFAULT);
-        prefNode.putBoolean(Constants.TEST_EXECUTION_RELEVANT_REMEMBER_KEY,
+        prefStore.setDefault(Constants.TEST_EXECUTION_RELEVANT_REMEMBER_KEY,
                 Constants.TEST_EXECUTION_RELEVANT_REMEMBER_KEY_DEFAULT);
     }
     
     /**
      * initialize the default preferences for a preference page 
-     * @param prefNode Preference node.
+     * @param prefStore Preference store
      */
     private static void initializeDefaultPreferencesObjectMapping(
-            IEclipsePreferences prefNode) {
-        prefNode.putBoolean(Constants.SHOWCHILDCOUNT_KEY,
+            IPreferenceStore prefStore) {
+        prefStore.setDefault(Constants.SHOWCHILDCOUNT_KEY,
                 Constants.SHOWCHILDCOUNT_KEY_DEFAULT);
     }
     
     /**
      * initialize the default preferences for a preference page 
-     * @param prefNode Preference node.
+     * @param prefStore Preference store
      */
     private static void initializeDefaultPreferencesKeyBoardShortCuts(
-            IEclipsePreferences prefNode) {
-        prefNode.putInt(Constants.MAPPING_MOD_KEY,
+            IPreferenceStore prefStore) {
+        prefStore.setDefault(Constants.MAPPING_MOD_KEY,
                 Constants.MAPPINGMOD1_KEY_DEFAULT);
-        prefNode.putInt(Constants.MAPPING_TRIGGER_KEY,
+        prefStore.setDefault(Constants.MAPPING_TRIGGER_KEY,
                 Constants.MAPPING_TRIGGER_DEFAULT);
-        prefNode.putInt(Constants.MAPPING_TRIGGER_TYPE_KEY,
+        prefStore.setDefault(Constants.MAPPING_TRIGGER_TYPE_KEY,
                 Constants.MAPPING_TRIGGER_TYPE_DEFAULT);
     }
     
     /**
      * initialize the default preferences for a preference page 
-     * @param prefNode Preference node.
+     * @param prefStore Preference store
      */
     private static void initializeDefaultPreferencesObservation(
-            IEclipsePreferences prefNode) {
-        prefNode.putInt(Constants.RECORDMOD_COMP_MODS_KEY,
+            IPreferenceStore prefStore) {
+        prefStore.setDefault(Constants.RECORDMOD_COMP_MODS_KEY,
                 Constants.RECORDMOD1_KEY_DEFAULT);
-        prefNode.putInt(Constants.RECORDMOD_COMP_KEY_KEY,
+        prefStore.setDefault(Constants.RECORDMOD_COMP_KEY_KEY,
                 Constants.RECORDMOD2_KEY_DEFAULT);
-        prefNode.putInt(Constants.RECORDMOD_APPL_MODS_KEY,
+        prefStore.setDefault(Constants.RECORDMOD_APPL_MODS_KEY,
                 Constants.RECORDMOD_APPL_MODS_DEFAULT);
-        prefNode.putInt(Constants.RECORDMOD_APPL_KEY_KEY,
+        prefStore.setDefault(Constants.RECORDMOD_APPL_KEY_KEY,
                 Constants.RECORDMOD_APPL_KEY_DEFAULT);        
         
-        prefNode.putInt(Constants.CHECKMODE_MODS_KEY,
+        prefStore.setDefault(Constants.CHECKMODE_MODS_KEY,
                 Constants.CHECKMODE_MODS_KEY_DEFAULT);
-        prefNode.putInt(Constants.CHECKMODE_KEY_KEY,
+        prefStore.setDefault(Constants.CHECKMODE_KEY_KEY,
                 Constants.CHECKMODE_KEY_KEY_DEFAULT);
-        prefNode.putInt(Constants.CHECKCOMP_MODS_KEY,
+        prefStore.setDefault(Constants.CHECKCOMP_MODS_KEY,
                 Constants.CHECKCOMP_MODS_KEY_DEFAULT);
-        prefNode.putInt(Constants.CHECKCOMP_KEY_KEY,
+        prefStore.setDefault(Constants.CHECKCOMP_KEY_KEY,
                 Constants.CHECKCOMP_KEY_KEY_DEFAULT);
         
-        prefNode.putBoolean(Constants.SHOWRECORDDIALOG_KEY,
+        prefStore.setDefault(Constants.SHOWRECORDDIALOG_KEY,
                 Constants.SHOWRECORDDIALOG_KEY_DEFAULT);
         
-        prefNode.put(Constants.SINGLELINETRIGGER_KEY,
+        prefStore.setDefault(Constants.SINGLELINETRIGGER_KEY,
                 Constants.SINGLELINETRIGGER_KEY_DEFAULT);
-        prefNode.put(Constants.MULTILINETRIGGER_KEY,
+        prefStore.setDefault(Constants.MULTILINETRIGGER_KEY,
                 Constants.MULTILINETRIGGER_KEY_DEFAULT);
-        
     }
-
 }
