925bf96d65df135dbffba9aaf74898bd70ea832c
Markus Tiede <Markus.Tiede@bredex.de>
2012-03-20 15:33:54 +0100

    Sprint task - version number increase for Jubula to 1.2.1 on juno branch.

--- org.eclipse.jubula.documentation.gen/script/texgen.sh
+++ org.eclipse.jubula.documentation.gen/script/texgen.sh
@@ -41,7 +41,7 @@ LANG="$2"
 TEMPL_DIR="$GDWS_DIR/org.eclipse.jubula.documentation.gen/templates"
 
 # classpath
-CL_PATH="$TEXGEN_DIR/target/org.eclipse.jubula.documentation.gen-1.2.0-SNAPSHOT.jar"
+CL_PATH="$TEXGEN_DIR/target/org.eclipse.jubula.documentation.gen-1.2.1-SNAPSHOT.jar"
 
 for i in `ls $TEXGEN_DIR/target/lib/*.jar`
 do
