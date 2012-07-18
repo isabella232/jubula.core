c21f52f334f41ffe48f7b15126bccc747643f669
Markus Tiede <Markus.Tiede@bredex.de>
2012-07-18 15:32:50 +0200

    Fix for issue http://bugs.eclipse.org/382170

--- org.eclipse.jubula.client.core/src/org/eclipse/jubula/client/core/functions/AbstractFunctionEvaluator.java
+++ org.eclipse.jubula.client.core/src/org/eclipse/jubula/client/core/functions/AbstractFunctionEvaluator.java
@@ -8,30 +8,28 @@ import org.eclipse.osgi.util.NLS;
 /**
  * Abstract base class for methods useful in Evaluators
  * 
- *
- * @author al
+ * @author BREDEX GmbH
  * @created Feb 22, 2012
- * @version $Revision: 1.1 $
  */
 public abstract class AbstractFunctionEvaluator 
     implements IFunctionEvaluator {
 
     /**
-     * @param arguments evaluate() parameter
-     * @param numParamsExpected how many parameters does the Evaluator expect
-     * @throws InvalidDataException if the are not exactly numParamsExpected
-     * parameters in arguments
+     * @param arguments
+     *            evaluate() parameter
+     * @param numParamsExpected
+     *            how many parameters does the Evaluator expect
+     * @throws InvalidDataException
+     *             if the are not exactly numParamsExpected parameters in
+     *             arguments
      */
-    protected void validateParamCount(String[] arguments, 
-        int numParamsExpected)
+    protected void validateParamCount(String[] arguments, int numParamsExpected)
         throws InvalidDataException {
-        if (arguments.length != 2) {
-            throw new InvalidDataException(
-                    NLS.bind(Messages.WrongNumFunctionArgs, 
-                            new Integer[] {
-                                numParamsExpected, arguments.length}),
+        if (arguments.length != numParamsExpected) {
+            throw new InvalidDataException(NLS.bind(
+                    Messages.WrongNumFunctionArgs, new Integer[] {
+                        numParamsExpected, arguments.length }),
                     MessageIDs.E_WRONG_NUM_FUNCTION_ARGS);
         }
     }
-
 }
