0d38089d73c4e06416bd1996227f6899c46fc964
Raimar Buehmann <raimar.buehmann@bredex.de>
2013-05-31 15:00:01 +0200

    Non sprint task: Marked old methods in ControlDecorator as deprecated

--- org.eclipse.jubula.client.ui.rcp/src/org/eclipse/jubula/client/ui/rcp/provider/ControlDecorator.java
+++ org.eclipse.jubula.client.ui.rcp/src/org/eclipse/jubula/client/ui/rcp/provider/ControlDecorator.java
@@ -23,36 +23,53 @@ import org.eclipse.swt.widgets.Control;
  * @author BREDEX GmbH
  * @created May 5, 2009
  */
-public class ControlDecorator {
-    
+public class ControlDecorator extends ControlDecoration {
+
     /**
-     * Private constructor for utility class.
+     * @param control The component for attaching the bobble.
+     * @param position The position of the bobble.
      */
-    private ControlDecorator() {
-        // nothing to initialize
+    public ControlDecorator(Control control, int position) {
+        super(control, position);
     }
-    
+
     /**
-     * @param control that should be decorated with an info-bobble
-     * @param position the position of the info-bobbles
-     * @param descriptionkey the i18n-key of the info-bobbles text description
-     * @param imageID the imageID for the image to show in this control decoration; 
+     * The following SWTException can be thrown:
+     * <ul>
+     *    <li>ERROR_WIDGET_DISPOSED - if the receiver has been disposed</li>
+     *    <li>ERROR_THREAD_INVALID_ACCESS - if not called from the thread that created the receiver</li>
+     * </ul>
+     * @param isVisible True, if the control is visible, otherwise the control
+     *        is hidden.
+     */
+    public void setVisible(boolean isVisible) {
+        getControl().setVisible(isVisible);
+    }
+
+    /**
+     * @param control The control for attaching the bobble.
+     * @param position The position of the bobble.
+     * @param message The message to show in the bobble.
+     * @param imageID The imageID of the image attached to the control;
      * see FieldDecorationRegistry constants for these ids
-     * @param showOnFocus set to true shows the info-bobble only if control has focus,
+     * @param showOnFocus set to true shows the bobble only if control has focus,
      * avoid setting this parameter to true if the control can not gain any focus
      * e.g. SWT.NO_FOCUS
+     * @return The created control decoration specified by the given parameters.
      */
-    protected static void decorate(Control control, int position,  
-            String descriptionkey, String imageID, Boolean showOnFocus) {
+    protected static ControlDecoration createDecoration(
+            Control control, int position,
+            String message, String imageID, boolean showOnFocus) {
         ControlDecoration infoBobbles = 
             new ControlDecoration(control, position);
-        infoBobbles.setDescriptionText(I18n.getString(descriptionkey));
+        infoBobbles.setDescriptionText(message);
         infoBobbles.setImage(FieldDecorationRegistry.getDefault()
                 .getFieldDecoration(imageID).getImage());
         infoBobbles.setMarginWidth(2);
         infoBobbles.setShowOnlyOnFocus(showOnFocus);
+        return infoBobbles;
     }
-    
+
     /**
      * 
      * @param control that should be decorated with an info-bobble
@@ -60,22 +77,48 @@ public class ControlDecorator {
      * @param showfocus set to true shows the info-bobble only if control has focus,
      * avoid setting this parameter to true if the control can not gain any focus
      * e.g. SWT.NO_FOCUS
+     * @deprecated Use {@link #createInfo(Control, String, Boolean).
      */
     public static void decorateInfo(Control control, 
             String descriptionkey, Boolean showfocus) { 
-        decorate(control, SWT.TRAIL, descriptionkey, 
-                FieldDecorationRegistry.DEC_INFORMATION, showfocus);
+        createInfo(control,
+                I18n.getString(descriptionkey), showfocus);
     }
 
     /**
      * 
-     * @param control contains the component which has an info-bobble
-     * @param position the position of the info-bobbles
-     * @param descriptionkey contains the text of the info-bobbles
+     * @param control that should be decorated with an info-bobble
+     * @param message The message to show in the info bobble.
+     * @param showfocus set to true shows the info-bobble only if control has focus,
+     * avoid setting this parameter to true if the control can not gain any focus
+     * e.g. SWT.NO_FOCUS
      */
-    public static void decorateWarning(Control control, int position, 
-            String descriptionkey) {
-        decorate(control, position, descriptionkey, 
+    public static void createInfo(Control control,
+            String message, boolean showfocus) {
+        createDecoration(control, SWT.TRAIL, message,
+                FieldDecorationRegistry.DEC_INFORMATION, showfocus);
+    }
+
+    /**
+     * @param control The control for adding the new warning icon for the bobble.
+     * @param message The message to show in the warning bobble.
+     * @return The created warning control decoration defined by the given parameters.
+     */
+    public static ControlDecoration createWarning(Control control,
+            String message) {
+        return createWarning(control, SWT.LEAD, message);
+    }
+
+    /**
+     * @param control The control for adding the new warning icon for the bobble.
+     * @param position The position for the new warning icon: SWT.TRAIL, SWT.LEAD.
+     * @param message The message to show in the warning bobble.
+     * @return The created warning control decoration defined by the given parameters.
+     */
+    public static ControlDecoration createWarning(Control control,
+            int position, String message) {
+        return createDecoration(control, position, message,
                 FieldDecorationRegistry.DEC_WARNING, false);
     }
+
 }
