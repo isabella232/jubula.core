b43c860653352af04262638872f9b519fa8fa019
Marvin Mueller <marvin.mueller@bredex.de>
2013-07-12 09:23:31 +0200

    Sprint task - Changing ShowServerLogAction to a command and handler

--- org.eclipse.jubula.client.ui.rcp/src/org/eclipse/jubula/client/ui/rcp/handlers/open/ShowServerLogHandler.java
+++ org.eclipse.jubula.client.ui.rcp/src/org/eclipse/jubula/client/ui/rcp/handlers/open/ShowServerLogHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2010 BREDEX GmbH.
+ * Copyright (c) 2013 BREDEX GmbH.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,68 +8,54 @@
  * Contributors:
  *     BREDEX GmbH - initial API and implementation and/or initial documentation
  *******************************************************************************/
-package org.eclipse.jubula.client.ui.rcp.actions;
+package org.eclipse.jubula.client.ui.rcp.handlers.open;
 
-import org.eclipse.jface.action.IAction;
-import org.eclipse.jubula.client.ui.rcp.businessprocess.AbstractActionBP;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.jubula.client.ui.handlers.AbstractHandler;
+import org.eclipse.jubula.client.ui.rcp.Plugin;
 import org.eclipse.jubula.client.ui.rcp.businessprocess.ShowServerLogBP;
 import org.eclipse.jubula.client.ui.rcp.editors.ISimpleEditorInput;
 import org.eclipse.jubula.client.ui.rcp.editors.ServerLogInput;
 import org.eclipse.jubula.client.ui.utils.ErrorHandlingUtil;
 import org.eclipse.jubula.communication.message.ServerLogResponseMessage;
 import org.eclipse.jubula.tools.messagehandling.MessageIDs;
-import org.eclipse.swt.widgets.Event;
 import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PartInitException;
 
-
 /**
+ * 
  * @author BREDEX GmbH
- * @created Feb 8, 2007
+ *
  */
-public class ShowServerLogAction extends AbstractAction {
+public class ShowServerLogHandler extends AbstractHandler {
 
-    /** The window associated with this action */
-    private IWorkbenchWindow m_window;
-    
     /** single instance of the ServerLogInput */
     private ISimpleEditorInput m_serverLogInput = null;
 
     /**
      * {@inheritDoc}
      */
-    public void init(IWorkbenchWindow window) {
-        m_window = window;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public void runWithEvent(IAction action, Event event) {
-        if (action != null && !action.isEnabled()) {
-            return;
-        }
-        
-        ServerLogResponseMessage response = 
-            ShowServerLogBP.getInstance().requestServerLog();
+    protected Object executeImpl(ExecutionEvent event) {
+        ServerLogResponseMessage response = ShowServerLogBP.getInstance()
+                .requestServerLog();
 
         if (response != null) {
             int status = response.getStatus();
             if (status == ServerLogResponseMessage.OK) {
-                IWorkbenchPage currentPage = m_window.getActivePage();
+                IWorkbenchPage currentPage = Plugin.getActivePage();
                 if (currentPage != null) {
-                    if (m_serverLogInput != null 
-                        && currentPage.findEditor(m_serverLogInput) != null) {
+                    if (m_serverLogInput != null
+                            && currentPage.findEditor(m_serverLogInput) 
+                            != null) {
                         currentPage.closeEditor(
                             currentPage.findEditor(m_serverLogInput), false);
                     }
-                    
-                    m_serverLogInput = 
-                        new ServerLogInput(response.getServerLog());
+
+                    m_serverLogInput = new ServerLogInput(
+                            response.getServerLog());
                     try {
-                        currentPage.openEditor(m_serverLogInput, 
-                            "org.eclipse.jubula.client.ui.rcp.editors.LogViewer"); //$NON-NLS-1$
+                        currentPage.openEditor(m_serverLogInput,
+                                        "org.eclipse.jubula.client.ui.rcp.editors.LogViewer"); //$NON-NLS-1$
                     } catch (PartInitException e) {
                         ErrorHandlingUtil.createMessageDialog(
                                 MessageIDs.E_CANNOT_OPEN_EDITOR);
@@ -77,13 +63,7 @@ public class ShowServerLogAction extends AbstractAction {
                 }
             }
         }
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    protected AbstractActionBP getActionBP() {
-        return ShowServerLogBP.getInstance();
+        return null;
     }
 
 }
