723de437a6ec9bf57dacd9ba99b44ca1eddbf1ea
Markus Tiede <Markus.Tiede@bredex.de>
2014-08-29 11:13:45 +0200

    Sprint task - RC API provisioning: artifact generation added + integration of first (independent) maven JUnit surefire test for general toolkit API.

---
+++ org.eclipse.jubula.qa.api/src/test/java/org/eclipse/jubula/qa/api/TestValueSets.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2014 BREDEX GmbH.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BREDEX GmbH - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+package org.eclipse.jubula.qa.api;
+
+import junit.framework.Assert;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/** @author BREDEX GmbH */
+public class TestValueSets {
+    /** @throws java.lang.Exception */
+    @Before
+    public void setUp() throws Exception {
+        // empty
+    }
+
+    /** @throws java.lang.Exception */
+    @After
+    public void tearDown() throws Exception {
+        // empty
+    }
+
+    /** the actual test method */
+    @Test
+    public void test() {
+        Assert.assertNull(null);
+    }
+}
