5802f27829c250b4e691c0d03818c3ecb0d988e0
Marvin Mueller <marvin.mueller@bredex.de>
2014-10-02 14:31:15 +0200

    Non-sprint task - add logging for not deletable testrun http://bugzilla.bredex.de/1542

--- org.eclipse.jubula.client.core/src/org/eclipse/jubula/client/core/persistence/TestResultSummaryPM.java
+++ org.eclipse.jubula.client.core/src/org/eclipse/jubula/client/core/persistence/TestResultSummaryPM.java
@@ -378,6 +378,7 @@ public class TestResultSummaryPM {
             }
             Persistor.instance().commitTransaction(session, tx);
         } catch (PMException e) {
+            log.error("Database exception while deleting testresults", e); //$NON-NLS-1$
             throw new JBFatalException(Messages.DeleteTestrunMetadataFailed, e,
                     MessageIDs.E_DATABASE_GENERAL);
         } catch (ProjectDeletedException e) {
