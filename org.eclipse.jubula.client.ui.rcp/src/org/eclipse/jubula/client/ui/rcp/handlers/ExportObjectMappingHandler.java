06b09c1e87c8679d6b1ce033c788eac732e09146
Sebastian Struckmann <sebastian.struckmann@bredex.de>
2014-10-16 13:27:20 +0200

    Sprint task - Java API - Prepared ExportMappingHandler for different types of exporting

--- org.eclipse.jubula.client.ui.rcp/src/org/eclipse/jubula/client/ui/rcp/handlers/ExportObjectMappingHandler.java
+++ org.eclipse.jubula.client.ui.rcp/src/org/eclipse/jubula/client/ui/rcp/handlers/ExportObjectMappingHandler.java
@@ -18,6 +18,7 @@ import java.util.SortedMap;
 import java.util.TreeMap;
 
 import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jubula.client.core.businessprocess.IComponentNameMapper;
 import org.eclipse.jubula.client.core.model.IAUTMainPO;
 import org.eclipse.jubula.client.core.model.IObjectMappingAssoziationPO;
@@ -59,43 +60,128 @@ public class ExportObjectMappingHandler extends AbstractHandler {
             final ObjectMappingMultiPageEditor omEditor = 
                 (ObjectMappingMultiPageEditor)activePart;
             IAUTMainPO aut = omEditor.getAut();
-            FileDialog saveDialog = new FileDialog(getActiveShell(), SWT.SAVE);
-            saveDialog.setFileName("objectMapping" + StringConstants.UNDERSCORE //$NON-NLS-1$
-                    + aut.getName() + ".properties"); //$NON-NLS-1$
-            saveDialog.setFilterExtensions(new String[] { "*.properties" }); //$NON-NLS-1$
-            saveDialog.setOverwrite(true);
-            String filterPath = Utils.getLastDirPath();
-            saveDialog.setFilterPath(filterPath);
-            String path = saveDialog.open();
-            if (path != null) {
-                Utils.storeLastDirPath(saveDialog.getFilterPath());
-                IObjectMappingPO objMap = aut.getObjMap();
-                IObjectMappingCategoryPO mappedCategory =
-                        objMap.getMappedCategory();
-                m_compMapper = omEditor.getEditorHelper().getEditSupport()
-                        .getCompMapper();
-                try {
-                    writeAssociationsToMap(objMap, mappedCategory);
-                } catch (LogicComponentNotManagedException | IOException e) {
-                    ErrorHandlingUtil.createMessageDialog(new JBException(e
-                            .getMessage(), e, MessageIDs.E_EXPORT_OM_ERROR));
-                }
-                // map is filled and can be written to file
-                try (BufferedWriter writer = new BufferedWriter(
-                        new FileWriter(path))) {
-                    for (String key : m_map.keySet()) {
-                        String value = m_map.get(key);
-                        writer.append(key + StringConstants.EQUALS_SIGN + value
-                                + StringConstants.NEWLINE);
+            
+            int exportType = determineExportType();
+            if (exportType != -1) {
+                FileDialog saveDialog = createSaveDialog(aut, exportType);
+                String path = saveDialog.open();
+                if (path != null) {
+                    Utils.storeLastDirPath(saveDialog.getFilterPath());
+                    fillMap(omEditor, aut);
+                    // map is filled and can be written to class or file
+                    StringBuffer encodedAssociations =
+                            generateEncodedAssociations();
+                    try (BufferedWriter writer = new BufferedWriter(
+                            new FileWriter(path))) {
+                        switch (exportType) {
+                            case 0: // Write Java Class
+                                writer.append(
+                                        generateJavaClass(encodedAssociations));
+                                break;
+                            case 1: // Write Properties File
+                                writer.append(encodedAssociations);
+                                break;
+                            default: // Nothing
+                                break;
+                        }
+                    } catch (IOException e) {
+                        ErrorHandlingUtil.createMessageDialog(
+                                new JBException(e.getMessage(), e,
+                                        MessageIDs.E_FILE_NO_PERMISSION));
                     }
-                } catch (IOException e) {
-                    ErrorHandlingUtil.createMessageDialog(
-                            new JBException(e.getMessage(), e,
-                                    MessageIDs.E_FILE_NO_PERMISSION));
                 }
             }
         }
         return null;
+    }
+
+    /**
+     * Generates a Java Class which allows access to all mapped components
+     * @param encodedAssociations the map containing the encoded associations
+     * @return the Java Class
+     */
+    private StringBuffer generateJavaClass(StringBuffer encodedAssociations) {
+        return new StringBuffer("This will be a pretty cool Java Class."); //$NON-NLS-1$
+    }
+
+    /**
+     * fills the map with the encoded object mapping associations
+     * @param omEditor the object mapping editor
+     * @param aut the aut
+     */
+    private void fillMap(final ObjectMappingMultiPageEditor omEditor,
+            IAUTMainPO aut) {
+        IObjectMappingPO objMap = aut.getObjMap();
+        IObjectMappingCategoryPO mappedCategory =
+                objMap.getMappedCategory();
+        m_compMapper = omEditor.getEditorHelper().getEditSupport()
+                .getCompMapper();
+        try {
+            writeAssociationsToMap(objMap, mappedCategory);
+        } catch (LogicComponentNotManagedException | IOException e) {
+            ErrorHandlingUtil.createMessageDialog(new JBException(e
+                    .getMessage(), e, MessageIDs.E_EXPORT_OM_ERROR));
+        }
+    }
+
+    /**
+     * Creates the save dialogue
+     * @param aut the aut (needed for the name)
+     * @param exportType the type of the export
+     * @return the save dialogue
+     */
+    private FileDialog createSaveDialog(IAUTMainPO aut, int exportType) {
+        String fileName = StringConstants.EMPTY;
+        String fileExtension = StringConstants.EMPTY;
+        switch (exportType) {
+            case 0: // Write Java Class
+                fileExtension = ".java"; //$NON-NLS-1$
+                break;
+            case 1: // Write Properties File
+                fileExtension = ".properties"; //$NON-NLS-1$
+                break;
+            default: // Nothing
+                break;
+        }
+        fileName = "objectMapping" + StringConstants.UNDERSCORE //$NON-NLS-1$
+                + aut.getName() + fileExtension;
+        
+        FileDialog saveDialog = new FileDialog(getActiveShell(), SWT.SAVE);
+        saveDialog.setFileName(fileName);
+        saveDialog.setFilterExtensions(
+                new String[] { StringConstants.STAR + fileExtension });
+        saveDialog.setOverwrite(true);
+        String filterPath = Utils.getLastDirPath();
+        saveDialog.setFilterPath(filterPath);
+        return saveDialog;
+    }
+
+    /**
+     * Opens a question dialogue to determine the desired export type
+     * @return the export type
+     *      <code>0</code> for a Java Class File
+     *      <code>1</code> for a Properties File
+     */
+    private int determineExportType() {
+        String dialogTitle = "Choose Export Type"; //$NON-NLS-1$
+        String dialogMessage = "What do you prefer?"; //$NON-NLS-1$
+        MessageDialog dialog = new MessageDialog(getActiveShell(), dialogTitle,
+                null, dialogMessage, MessageDialog.QUESTION, new String[] {
+                    "Java Class", "Properties File" }, 0); //$NON-NLS-1$ //$NON-NLS-2$
+        return dialog.open();
+    }
+
+    /**
+     * @return StringBuffer containing the map with the encoded object mappings
+     */
+    private StringBuffer generateEncodedAssociations() {
+        StringBuffer content = new StringBuffer();
+        for (String key : m_map.keySet()) {
+            String value = m_map.get(key);
+            content.append(key + StringConstants.EQUALS_SIGN + value
+                    + StringConstants.NEWLINE);
+        }
+        return content;
     }
 
     /**
