b38b70e0b57c7b941dbe3782b5eac016e8946b6b
Markus Tiede <Markus.Tiede@bredex.de>
2014-11-12 15:11:41 +0100

    Non-sprint task - allow src node UUID backlinking via annotation + class comment conversion added.

---
+++ org.eclipse.jubula.client.api.converter.annotations/src/org/eclipse/jubula/client/api/converter/annotations/SrcNode.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2014 BREDEX GmbH.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BREDEX GmbH - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+package org.eclipse.jubula.client.api.converter.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation of converter source node attribute values
+ * 
+ * @author BREDEX GmbH
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface SrcNode {
+    /** returns the UUID */
+    String UUID();
+    /** returns the node type */
+    NodeType type();
+}
