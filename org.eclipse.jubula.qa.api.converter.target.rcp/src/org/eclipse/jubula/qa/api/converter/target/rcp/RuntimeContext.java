c318f8785ee3e00b16bce022c13cb69bbd3c98c7
Markus Tiede <Markus.Tiede@bredex.de>
2014-11-19 15:14:17 +0100

    Sprint task - hide setter for AUT in used runtime context.

--- org.eclipse.jubula.qa.api.converter.target.rcp/src/org/eclipse/jubula/qa/api/converter/target/rcp/RuntimeContext.java
+++ org.eclipse.jubula.qa.api.converter.target.rcp/src/org/eclipse/jubula/qa/api/converter/target/rcp/RuntimeContext.java
@@ -13,32 +13,31 @@ package org.eclipse.jubula.qa.api.converter.target.rcp;
 import org.eclipse.jubula.client.AUT;
 
 /**
- *  @created 19.11.2014
+ * @created 19.11.2014
  */
 public class RuntimeContext {
-    
     /** the AUT */
     private AUT m_aut;
-    
+
     /**
-     * @param aut the AUT
+     * @param aut
+     *            the AUT
      */
     public RuntimeContext(AUT aut) {
-        m_aut = aut;
+        setAUT(aut);
     }
-    
+
     /**
      * @return the AUT
      */
     public AUT getAUT() {
         return m_aut;
     }
-    
+
     /**
-     * Changes the AUT during runtime
      * @param aut the AUT to set
      */
-    public void setAUT(AUT aut) {
+    private void setAUT(AUT aut) {
         m_aut = aut;
     }
 }
