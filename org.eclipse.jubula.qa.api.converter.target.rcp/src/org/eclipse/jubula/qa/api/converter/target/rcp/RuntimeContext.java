a19adf670b2f1131c058f7b13025857999994672
Markus Tiede <Markus.Tiede@bredex.de>
2014-11-24 16:10:27 +0100

    Sprint task - add support for event handling ignoring of check failed exceptions.

--- org.eclipse.jubula.qa.api.converter.target.rcp/src/org/eclipse/jubula/qa/api/converter/target/rcp/RuntimeContext.java
+++ org.eclipse.jubula.qa.api.converter.target.rcp/src/org/eclipse/jubula/qa/api/converter/target/rcp/RuntimeContext.java
@@ -10,14 +10,47 @@
  *******************************************************************************/
 package org.eclipse.jubula.qa.api.converter.target.rcp;
 
+import java.util.Stack;
+
 import org.eclipse.jubula.client.AUT;
 import org.eclipse.jubula.client.ObjectMapping;
+import org.eclipse.jubula.client.exceptions.CheckFailedException;
+import org.eclipse.jubula.client.exceptions.ExecutionException;
+import org.eclipse.jubula.client.exceptions.ExecutionExceptionHandler;
 import org.eclipse.jubula.tools.ComponentIdentifier;
 
 /**
  * @created 19.11.2014
  */
 public class RuntimeContext {
+    private static class CheckFailedExecutionHandler 
+        implements ExecutionExceptionHandler {
+        /** nesting level counter */
+        private Stack<Boolean> m_stack = new Stack<Boolean>();
+
+        /**
+         * @param defaultHandling
+         *            the default handling
+         */
+        public CheckFailedExecutionHandler(Boolean defaultHandling) {
+            getStack().push(defaultHandling);
+        }    
+        
+        /** special handling supports ignoring of check failed exceptions */
+        public void handle(ExecutionException arg0) throws ExecutionException {
+            if ((arg0 instanceof CheckFailedException) && getStack().peek()) {
+                return;
+            }
+            throw arg0;
+        }
+
+        /**
+         * @return the stack
+         */
+        public Stack<Boolean> getStack() {
+            return m_stack;
+        }
+    }
     
     /** the AUT */
     private AUT m_aut;
@@ -25,13 +58,17 @@ public class RuntimeContext {
     /** the object map to use */
     private ObjectMapping om;
 
+    /** the event handler for this runtime context */
+    private CheckFailedExecutionHandler m_eventHandler;
+
     /**
      * @param aut
      *            the AUT
      */
-    public RuntimeContext(AUT aut) {
+    public RuntimeContext(AUT aut, boolean defaultHandling) {
         setAUT(aut);
-        
+        m_eventHandler = new CheckFailedExecutionHandler(defaultHandling);
+        aut.setHandler(m_eventHandler);
         // TODO: load OM for the AUT
     }
 
@@ -57,5 +94,35 @@ public class RuntimeContext {
      */
     public ComponentIdentifier getIdentifier(String name) {
         return om.get(name);
+    }
+    
+    /**
+     * activate ignoring of
+     * {@link org.eclipse.jubula.client.exceptions.CheckFailedException}
+     */
+    public void beginIgnoreCheckFailed() {
+        getEventStack().push(true);
+    }
+
+    /**
+     * @return the current event stack
+     */
+    private Stack<Boolean> getEventStack() {
+        return m_eventHandler.getStack();
+    }
+
+    /**
+     * end local event handling and restore previous state 
+     */
+    public void endLocalEventHandling() {
+        getEventStack().pop();
+    }
+    
+    /**
+     * deactivate ignoring of
+     * {@link org.eclipse.jubula.client.exceptions.CheckFailedException}
+     */
+    public void doNotIgnoreCheckFailed() {
+        getEventStack().push(false);
     }
 }
