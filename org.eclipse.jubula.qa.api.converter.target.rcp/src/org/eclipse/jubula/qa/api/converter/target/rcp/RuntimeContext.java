3ef39a60cac0794f6b6d4190c8b8e4b6de94b37c
Sebastian Struckmann <sebastian.struckmann@bredex.de>
2014-11-24 14:55:29 +0100

    Sprint task - Java API - Added loading of component identifiers from object mapping to converter

--- org.eclipse.jubula.qa.api.converter.target.rcp/src/org/eclipse/jubula/qa/api/converter/target/rcp/RuntimeContext.java
+++ org.eclipse.jubula.qa.api.converter.target.rcp/src/org/eclipse/jubula/qa/api/converter/target/rcp/RuntimeContext.java
@@ -11,13 +11,19 @@
 package org.eclipse.jubula.qa.api.converter.target.rcp;
 
 import org.eclipse.jubula.client.AUT;
+import org.eclipse.jubula.client.ObjectMapping;
+import org.eclipse.jubula.tools.ComponentIdentifier;
 
 /**
  * @created 19.11.2014
  */
 public class RuntimeContext {
+    
     /** the AUT */
     private AUT m_aut;
+    
+    /** the object map to use */
+    private ObjectMapping om;
 
     /**
      * @param aut
@@ -25,6 +31,8 @@ public class RuntimeContext {
      */
     public RuntimeContext(AUT aut) {
         setAUT(aut);
+        
+        // TODO: load OM for the AUT
     }
 
     /**
@@ -39,5 +47,15 @@ public class RuntimeContext {
      */
     private void setAUT(AUT aut) {
         m_aut = aut;
+    }
+
+    /**
+     * Gets a component identifier for a given logical component name 
+     * from the object mapping for the AUT
+     * @param name the logical component name
+     * @return the component identifier
+     */
+    public ComponentIdentifier getIdentifier(String name) {
+        return om.get(name);
     }
 }
