4a1bbdd1b2ceeee7e05573b047b9cb24fd4713fb
Markus Tiede <Markus.Tiede@bredex.de>
2015-01-16 16:11:37 +0100

    Sprint task - fix for issue http://eclip.se/457573

--- org.eclipse.jubula.client.ui.rcp/src/org/eclipse/jubula/client/ui/rcp/properties/ProjectALMPropertyPage.java
+++ org.eclipse.jubula.client.ui.rcp/src/org/eclipse/jubula/client/ui/rcp/properties/ProjectALMPropertyPage.java
@@ -81,13 +81,19 @@ public class ProjectALMPropertyPage extends AbstractProjectPropertyPage {
         /** {@inheritDoc} */
         public void widgetSelected(SelectionEvent e) {
             String selectedObject = m_almRepoCombo.getSelectedObject();
-            IStatus connectionStatus = ALMAccess.testConnection(selectedObject);
-            if (connectionStatus.isOK()) {
-                m_connectionTest.setImage(IconConstants.STEP_OK_IMAGE);
-                setErrorMessage(null);
+            if (selectedObject != null) {
+                IStatus connectionStatus = ALMAccess.testConnection(
+                        selectedObject);
+                if (connectionStatus.isOK()) {
+                    m_connectionTest.setImage(IconConstants.STEP_OK_IMAGE);
+                    setErrorMessage(null);
+                } else {
+                    m_connectionTest.setImage(IconConstants.ERROR_IMAGE);
+                    setErrorMessage(connectionStatus.getMessage());
+                }
             } else {
-                m_connectionTest.setImage(IconConstants.ERROR_IMAGE);
-                setErrorMessage(connectionStatus.getMessage());
+                m_connectionTest.setImage(null);
+                setErrorMessage(null);
             }
         }
         
