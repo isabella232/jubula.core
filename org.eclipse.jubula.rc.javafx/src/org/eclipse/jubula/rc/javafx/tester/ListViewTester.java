a27dc4b37e51a9a1433a3e78cd74662f5ec4ac57
Sebastian Struckmann <sebastian.struckmann@bredex.de>
2015-03-04 14:58:46 +0100

    Sprint task - Extended JavaFX RC toolkit to support Drag&Drop on lists

--- org.eclipse.jubula.rc.javafx/src/org/eclipse/jubula/rc/javafx/tester/ListViewTester.java
+++ org.eclipse.jubula.rc.javafx/src/org/eclipse/jubula/rc/javafx/tester/ListViewTester.java
@@ -10,8 +10,9 @@
  *******************************************************************************/
 package org.eclipse.jubula.rc.javafx.tester;
 
-import org.eclipse.jubula.rc.common.exception.StepExecutionException;
+import org.eclipse.jubula.rc.common.driver.DragAndDropHelper;
 import org.eclipse.jubula.rc.common.tester.ListTester;
+import org.eclipse.jubula.toolkit.enums.ValueSets.BinaryChoice;
 
 /**
  * @author BREDEX GmbH
@@ -20,22 +21,50 @@ public class ListViewTester extends ListTester {
     @Override
     public void rcDragValue(int mouseButton, String modifier, String value,
         String operator, String searchType) {
-        StepExecutionException.throwUnsupportedAction();
+        final DragAndDropHelper dndHelper = DragAndDropHelper.getInstance();
+        dndHelper.setModifier(modifier);
+        dndHelper.setMouseButton(mouseButton);
+        rcSelectValue(value, operator, searchType, BinaryChoice.no.rcValue(),
+                mouseButton, 0);
+        pressOrReleaseModifiers(modifier, true);
+        getRobot().mousePress(null, null, mouseButton);
     }
 
     @Override
     public void rcDropValue(String value, String operator, String searchType,
         int delayBeforeDrop) {
-        StepExecutionException.throwUnsupportedAction();
+        final DragAndDropHelper dndHelper = DragAndDropHelper.getInstance();
+        try {
+            rcSelectValue(value, operator, searchType,
+                    BinaryChoice.no.rcValue(), dndHelper.getMouseButton(), 0);
+            waitBeforeDrop(delayBeforeDrop);
+        } finally {
+            getRobot().mouseRelease(null, null, dndHelper.getMouseButton());
+            pressOrReleaseModifiers(dndHelper.getModifier(), false);
+        }
     }
 
     @Override
     public void rcDragIndex(int mouseButton, String modifier, int index) {
-        StepExecutionException.throwUnsupportedAction();
+        final DragAndDropHelper dndHelper = DragAndDropHelper.getInstance();
+        dndHelper.setModifier(modifier);
+        dndHelper.setMouseButton(mouseButton);
+        rcSelectIndex(String.valueOf(index), BinaryChoice.no.rcValue(),
+                mouseButton, 0);
+        pressOrReleaseModifiers(modifier, true);
+        getRobot().mousePress(null, null, mouseButton);
     }
 
     @Override
     public void rcDropIndex(int index, int delayBeforeDrop) {
-        StepExecutionException.throwUnsupportedAction();
+        final DragAndDropHelper dndHelper = DragAndDropHelper.getInstance();
+        try {
+            rcSelectIndex(String.valueOf(index), BinaryChoice.no.rcValue(),
+                    dndHelper.getMouseButton(), 0);
+            waitBeforeDrop(delayBeforeDrop);
+        } finally {
+            getRobot().mouseRelease(null, null, dndHelper.getMouseButton());
+            pressOrReleaseModifiers(dndHelper.getModifier(), false);
+        }
     }
 }
