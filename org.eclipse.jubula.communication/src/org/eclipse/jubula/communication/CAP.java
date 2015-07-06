9a9c4a0535a5819b59bc2546f8fe58aa073cac55
Sebastian Struckmann <sebastian.struckmann@bredex.de>
2015-07-06 11:54:14 +0200

    Sprint task - Move CapBuilder from o.e.j.communication to o.e.j.toolkit.api

--- org.eclipse.jubula.communication/src/org/eclipse/jubula/communication/CAP.java
+++ org.eclipse.jubula.communication/src/org/eclipse/jubula/communication/CAP.java
@@ -10,13 +10,7 @@
  *******************************************************************************/
 package org.eclipse.jubula.communication;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.eclipse.jubula.communication.internal.message.MessageCap;
-import org.eclipse.jubula.communication.internal.message.MessageParam;
 import org.eclipse.jubula.tools.ComponentIdentifier;
-import org.eclipse.jubula.tools.internal.objects.IComponentIdentifier;
 
 /**
  * CAPs are executed remotely on the {@link org.eclipse.jubula.client.AUT AUT}<br>
@@ -36,110 +30,4 @@ public interface CAP {
      *         mapping
      */
     ComponentIdentifier getComponentIdentifier();
-    
-    /**
-     * Class following builder pattern to create CAP instances
-     * @author BREDEX GmbH
-     */
-    public static class Builder {
-        
-        /** Identifier for Boolean parameters */
-        private static final String BOOLEAN_IDENTIFIER = Boolean.class
-                .getName();
-
-        /** Identifier for Integer parameters */
-        private static final String INTEGER_IDENTIFIER = Integer.class
-                .getName();
-
-        /** Identifier for String parameters */
-        private static final String STRING_IDENTIFIER = String.class.getName();
-
-        /** the name of the method */
-        private String m_rcMethod;
-        
-        /** the component identifier */
-        private IComponentIdentifier m_ci = null;
-        
-        /** the parameters of the CAP */
-        private List<MessageParam> m_params =
-                new ArrayList<MessageParam>();
-
-        /** whether component has default mapping */
-        private boolean m_defaultMapping = true;
-        
-        /**
-         * Builder for creating CAPs
-         * @param rcMethod name of the rc method of the CAP
-         */
-        public Builder(String rcMethod) {
-            m_rcMethod = rcMethod;
-        }
-        
-        /**
-         * @param value the value of the string parameter
-         * @return the modified builder
-         */
-        public Builder addParameter(String value) {
-            m_params.add(new MessageParam(String.valueOf(value),
-                    STRING_IDENTIFIER));
-            return this;
-        }
-        
-        /**
-         * @param value the value of the integer parameter
-         * @return the modified builder
-         */
-        public Builder addParameter(Integer value) {
-            m_params.add(new MessageParam(String.valueOf(value),
-                    INTEGER_IDENTIFIER));
-            return this;
-        }
-        
-        /**
-         * @param value the value of the boolean parameter
-         * @return the modified builder
-         */
-        public Builder addParameter(Boolean value) {
-            m_params.add(new MessageParam(String.valueOf(value),
-                    BOOLEAN_IDENTIFIER));
-            return this;
-        }
-        
-        /**
-         * Sets the component identifier.
-         * @param ci the component identifier
-         * @return the modified builder
-         */
-        public Builder setComponentIdentifier(IComponentIdentifier ci) {
-            m_ci = ci;
-            return this;
-        }
-        
-        /**
-         * Sets whether the component has a default mapping or not
-         * (Default value is <code>true</code>).
-         * @param defaultMapping whether the component has default mapping
-         * @return the modified builder
-         */
-        public Builder setDefaultMapping(Boolean defaultMapping) {
-            m_defaultMapping = defaultMapping;
-            return this;
-        }
-        
-        /**
-         * build CAP instance
-         * @return CAP instance
-         */
-        public CAP build() {
-            MessageCap messageCap = new MessageCap();
-            messageCap.setMethod(m_rcMethod);
-            messageCap.sethasDefaultMapping(m_defaultMapping);
-            messageCap.setCi(m_ci);
-            for (MessageParam param : m_params) {
-                messageCap.addMessageParam(param);
-            }
-            return messageCap;
-        }
-
-    }
 }
