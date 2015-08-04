f535ef4135d17f8b54618654e68fe697bc055e47
Marvin Mueller <marvin.mueller@bredex.de>
2015-08-04 16:36:43 +0200

    Non-sprint task - add environment variable for color in objectmapping modus(javafX)

--- org.eclipse.jubula.rc.javafx/src/org/eclipse/jubula/rc/javafx/util/HighlightNode.java
+++ org.eclipse.jubula.rc.javafx/src/org/eclipse/jubula/rc/javafx/util/HighlightNode.java
@@ -13,6 +13,10 @@ package org.eclipse.jubula.rc.javafx.util;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.eclipse.jubula.tools.internal.utils.EnvironmentUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javafx.scene.Node;
 import javafx.scene.effect.Effect;
 import javafx.scene.effect.InnerShadow;
@@ -26,8 +30,25 @@ import javafx.scene.paint.Color;
  */
 public class HighlightNode {
 
+    /** for log messages */
+    private static Logger log = LoggerFactory.getLogger(HighlightNode.class);
+    
+    /**
+     * Name of the environment variable that defines the color
+     * of the shadow for the object mapping
+     */
+    private static final String OBJECT_MAPPING_COLOR_VAR = "TEST_OBJECT_MAPPING_COLOR"; //$NON-NLS-1$
+    
+    
     /** The old effect of the Node **/
     private static Map<Node, Effect> oldEffects = new HashMap<>();
+    
+    /** the color of the shadow
+     * default is {@link Color#GREEN}
+     * you can set your own color with {@link #OBJECT_MAPPING_COLOR_VAR} as environment or system property and a string.
+     * This should have the format as seen in {@link Color#web(String)}
+     */
+    private static Color shadowColor = Color.GREEN;
 
     /**
      * private Constructor
@@ -36,6 +57,17 @@ public class HighlightNode {
         // private Constructor
     }
 
+    static {
+        try {
+            String value = EnvironmentUtils
+                    .getProcessOrSystemProperty(OBJECT_MAPPING_COLOR_VAR);
+            if (value != null) {
+                shadowColor = Color.valueOf(value);
+            }
+        } catch (Exception e) {
+            log.warn("Could not parse color for object mapping", e); //$NON-NLS-1$
+        }
+    }
     /**
      * Use this method only from the FX-Thread! Draws a border around the given
      * Node
@@ -52,7 +84,8 @@ public class HighlightNode {
         if (n.getEffect() != null) {
             oldEffects.put(n, n.getEffect());
         }
-        n.setEffect(new InnerShadow(10, Color.GREEN));
+
+        n.setEffect(new InnerShadow(10, shadowColor));
     }
 
     /**
