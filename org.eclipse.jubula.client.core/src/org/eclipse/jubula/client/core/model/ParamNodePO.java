1f538bfff2915e607190a1143058d9d9bfdda120
Marvin Mueller <marvin.mueller@bredex.de>
2016-03-10 07:30:05 +0100

    Non-sprint task - fix for Bug 489220
    Due to a problem with the loading of some structures and therefore
    having doubled datasets in the TDManager removing the batch fetching is
    necessary
    see http://eclip.se/489220 for more information
    
    Change-Id: Icefa77d1330d0e95c7ab64f6daf4fb5401a39f80

--- org.eclipse.jubula.client.core/src/org/eclipse/jubula/client/core/model/ParamNodePO.java
+++ org.eclipse.jubula.client.core/src/org/eclipse/jubula/client/core/model/ParamNodePO.java
@@ -29,8 +29,6 @@ import org.eclipse.jubula.client.core.businessprocess.TestDataBP;
 import org.eclipse.jubula.client.core.utils.ModelParamValueConverter;
 import org.eclipse.jubula.client.core.utils.ParamValueConverter;
 import org.eclipse.jubula.client.core.utils.RefToken;
-import org.eclipse.persistence.annotations.BatchFetch;
-import org.eclipse.persistence.annotations.BatchFetchType;
 
 
 
@@ -83,7 +81,8 @@ abstract class ParamNodePO extends NodePO implements IParamNodePO {
                targetEntity = TestDataCubePO.class, 
                fetch = FetchType.EAGER)
     @JoinColumn(name = "FK_PARAM_INTERFACE", unique = true)
-    @BatchFetch(value = BatchFetchType.JOIN)
+    // Do not use Batch fetching here this might lead to duplicate data
+    // <and> problems that are caused by that fact see e.g. http://eclip.se/489220
     private TestDataCubePO getParameterInterface() {
         return m_parameterInterface;
     }
