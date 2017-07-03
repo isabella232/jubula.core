193931b6372930d900fa1d5c81e59f5b74bbd2b5
Marvin Mueller <marvin.mueller@bredex.de>
2017-07-03 09:39:15 +0200

    Fix scrolling and NoSuchMethod error for TabbedPropertiesList
    Change-Id: I22694924ddb1716d53575abb2ad8e02ff5135801

--- org.eclipse.jubula.rc.rcp.swt/src/org/eclipse/jubula/rc/rcp/tabbedproperties/adapter/TabbedPropertiesAdapter.java
+++ org.eclipse.jubula.rc.rcp.swt/src/org/eclipse/jubula/rc/rcp/tabbedproperties/adapter/TabbedPropertiesAdapter.java
@@ -13,15 +13,23 @@
  */
 package org.eclipse.jubula.rc.rcp.tabbedproperties.adapter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.jubula.rc.common.driver.ClickOptions;
+import org.eclipse.jubula.rc.common.driver.IRunnable;
 import org.eclipse.jubula.rc.common.exception.StepExecutionException;
 import org.eclipse.jubula.rc.common.tester.adapter.interfaces.IListComponent;
 import org.eclipse.jubula.rc.common.util.SelectionUtil;
 import org.eclipse.jubula.rc.swt.tester.adapter.ControlAdapter;
-import org.eclipse.swt.widgets.Canvas;
-import org.eclipse.swt.widgets.TabItem;
+import org.eclipse.jubula.tools.internal.constants.StringConstants;
+import org.eclipse.jubula.tools.internal.objects.event.EventFactory;
+import org.eclipse.jubula.tools.internal.objects.event.TestErrorEvent;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.ui.internal.views.properties.tabbed.view.TabbedPropertyList;
-import org.eclipse.ui.internal.views.properties.tabbed.view.TabbedPropertyList.ListElement;
+import org.eclipse.ui.internal.views.properties.tabbed.view.TabbedPropertyList.BottomNavigationElement;
+import org.eclipse.ui.internal.views.properties.tabbed.view.TabbedPropertyList.TopNavigationElement;
 
 /**
  * The adapter for the {@link TabbedPropertyList}
@@ -77,34 +85,109 @@ public class TabbedPropertiesAdapter extends ControlAdapter
     /**
      * {@inheritDoc}
      */
-    public void clickOnIndex(Integer i, ClickOptions co) {
-        Canvas a = (Canvas) m_list.getElementAt(i.intValue());
-        TabItem c = null;
-        getRobot().click(a, null, co);
+    public void clickOnIndex(final Integer index, final ClickOptions co) {
+        Object element = scrollToElement(index);
+        getRobot().click(element, null, co);
         
+    }
+
+    /**
+     * @param target target number
+     * @return the wanted element
+     */
+    private Object scrollToElement(Integer target) {
+        final Control wantedElement =
+                (Control) m_list.getElementAt(target.intValue());
+        if (wantedElement == null) {
+            throw new StepExecutionException(
+                    "List index '" + target.intValue() //$NON-NLS-1$
+                            + "' is out of range", //$NON-NLS-1$
+                    EventFactory
+                            .createActionError(TestErrorEvent.INVALID_INDEX));
+        }
+        TopNavigationElement top = null;
+        BottomNavigationElement bottom = null;
+        int firstVisible = -1;
+        Control[] children = getChildren(m_list);
+        for (int j = 0; j < children.length; j++) {
+            Control control = children[j];
+            if (control instanceof TopNavigationElement) {
+                top = (TopNavigationElement) control;
+            } else if (control instanceof BottomNavigationElement) {
+                bottom = (BottomNavigationElement) control;
+            }
+            if (top != null && bottom != null) {
+                break;
+            }
+        }
+        int i = 0;
+        while (m_list.getElementAt(i) != null) {
+            Control element = (Control) m_list.getElementAt(i);
+            if (isVisible(element)) {
+                firstVisible = i;
+                break;
+            }
+            i++;
+        }
+        ClickOptions options = new ClickOptions().setClickCount(1);
+        int maxCount = getValues().length;
+        int j = 0;
+        while (!isVisible(wantedElement) &&  j <= maxCount) {
+            if (firstVisible > target) {
+                if (isVisible(top)) {
+                    getRobot().click(top, null, options);
+                } else {
+                    break;
+                }
+            } else {
+                if (isVisible(bottom)) {
+                    getRobot().click(bottom, null, options);
+                } else {
+                    break;
+                }
+            }
+            j++;
+        }
+        return wantedElement;
     }
 
     /**
      * {@inheritDoc}
      */
     public String[] getSelectedValues() {
-        ListElement element =
-                (ListElement) m_list.getElementAt(m_list.getSelectionIndex());
-        return new String[] { element.getTabItem().getText() };
+        return new String[] {
+                getTextOfElementAtIndex(
+                        m_list.getSelectionIndex()) };
     }
 
     /**
      * {@inheritDoc}
      */
     public String[] getValues() {
-        String[] values = new String[m_list.getNumberOfElements()];
-        for (int j = 0; j < m_list.getNumberOfElements(); j++) {
-            ListElement element = (ListElement) m_list.getElementAt(j);
-            String string = element.getTabItem().getText();
-            values[j] = string;
+        List<String> values = new ArrayList<String>();
+        try {
+            for (int i = 0; i < m_list.getNumberOfElements(); i++) {
+                values.add(getTextOfElementAtIndex(i));
+            }
+        } catch (NoSuchMethodError e) {
+            // 3.5.200 and older versions does not have the Method getNumberOfElements
+            int i = 0;
+            while (m_list.getElementAt(i) != null) {
+                values.add(getTextOfElementAtIndex(i));
+                i++;
+            }
         }
-        
-        return values;
+        return values.toArray(new String[values.size()]);
+    }
+
+    /**
+     *
+     * @param i the index
+     * @return the text of the
+     */
+    private String getTextOfElementAtIndex(int i) {
+        Object element = m_list.getElementAt(i);
+        return element != null ? element.toString() : StringConstants.EMPTY;
     }
 
     /**
@@ -115,4 +198,30 @@ public class TabbedPropertiesAdapter extends ControlAdapter
         return null;
     }
     
+    /**
+     * Wrapper for {@link Composite#getChildren()}
+     * @param c the composite
+     * @return the children of the composite
+     */
+    private Control[] getChildren(final Composite c) {
+        return getEventThreadQueuer().invokeAndWait("getChildren", new IRunnable<Control[]>() { //$NON-NLS-1$
+            public Control[] run() throws StepExecutionException {
+                return c.getChildren();
+            }
+        });
+    }
+
+    /**
+     * Wrapper for {@link Control#isVisible()}
+     * @param c the control
+     * @return is the control visible
+     */
+    private boolean isVisible(final Control c) {
+        return getEventThreadQueuer().invokeAndWait("isVisible", new IRunnable<Boolean>() { //$NON-NLS-1$
+            public Boolean run() throws StepExecutionException {
+                return c.isVisible();
+            }
+        });
+    }
+
 }
