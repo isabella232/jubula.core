8de4510bdcd806df350cd4da1af01a62470f1996
Marvin Mueller <marvin.mueller@bredex.de>
2017-07-11 14:11:12 +0200

    fix missing headers
    Change-Id: I96d012b2a26468be7bdbb0b984a7a08078a43b6d

--- org.eclipse.jubula.client.core/src/org/eclipse/jubula/client/core/testresult/export/ITestResultExporter.java
+++ org.eclipse.jubula.client.core/src/org/eclipse/jubula/client/core/testresult/export/ITestResultExporter.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2017 BREDEX GmbH.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BREDEX GmbH - initial API and implementation 
+ *******************************************************************************/
 package org.eclipse.jubula.client.core.testresult.export;
 
 import org.eclipse.jubula.client.core.model.ITestResult;
